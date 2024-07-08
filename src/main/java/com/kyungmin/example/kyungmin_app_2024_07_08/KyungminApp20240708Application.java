ccf5f9e7d635aad8bf69d9708595c356cd9a2478
ohkm9926 <ohkm9926@naver.com>
2024-07-08 11:08:51 +0900

    세팅

---
+++ src/main/java/com/kyungmin/example/kyungmin_app_2024_07_08/KyungminApp20240708Application.java
@@ -0,0 +1,13 @@
+package com.kyungmin.example.kyungmin_app_2024_07_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyungminApp20240708Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyungminApp20240708Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/kyungmin/example/kyungmin_app_2024_07_08/KyungminApp20240708ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyungmin.example.kyungmin_app_2024_07_08;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyungminApp20240708ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
